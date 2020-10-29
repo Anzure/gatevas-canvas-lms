7af20f164e4e575869b46f742f1d54f063219d60
André Mathisen <9752808+anzure@users.noreply.github.com>
2020-10-29 14:11:29 +0100

    Custom command

--- src/main/java/no/odit/gatevas/command/CustomCommand.java
+++ src/main/java/no/odit/gatevas/command/CustomCommand.java
@@ -3,6 +3,7 @@ package no.odit.gatevas.command;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Set;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,75 +28,78 @@ import no.odit.gatevas.service.StudentService;
 public class CustomCommand implements CommandHandler {
 
 	@Autowired
-	private Scanner commandScanner;
-
-	@Autowired
 	private StudentService studentService;
 
 	public void handleCommand(Command cmd) {
-		//		String[] args = cmd.getArgs();
-		//
-		//		if (args.length != 1) {
-		//			System.out.println("Mising argument!");
-		//			return;
-		//		}
-		//
-		//		if (args[0].equalsIgnoreCase("")) {
-		//			
-		//		}
 
 		try {
 
-			FileReader in = new FileReader(new File(""));
-			FileWriter out = new FileWriter(new File(""));
+			File in = new File("C:\\Users\\andre\\Documents\\input.csv");
+			FileWriter out = new FileWriter(new File("C:\\Users\\andre\\Documents\\output_1.csv"));
 
-			CSVParser parser = new CSVParser(in, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+			out.write('\ufeff');
 
-			Map<String, Integer> headerMap = parser.getHeaderMap();
-			String[] headerList = headerMap.keySet().toArray(new String[0]);
+			CSVParser parser = CSVParser.parse(in, StandardCharsets.ISO_8859_1, CSVFormat.DEFAULT.withAllowMissingColumnNames().withDelimiter(';').withFirstRecordAsHeader());
 
-			CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(headerList));
+			String[] header = {"Tidsmerke", "E-postadresse", "Jeg melder meg på følgende del- utdanning", "Arbeidsstatus", "Bransjetilhørighet",
+					"Etternavn", "Fornavn", "Adresse privat", "Postnr og sted (Skriv begge deler)",
+					"Antall år i bransjen", "Tlf nr", "Fødselsdato ", "Status", "Kommentarer"};
+			CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withAllowMissingColumnNames().withDelimiter(';').withHeader(header));
 
-			parser.forEach(row -> {
+			for (CSVRecord row : parser.getRecords()) {
 
+				String time = row.get("Tidsmerke");
 				String email = row.get("E-postadresse");
-				String firstName = row.get("Fornavn");
-				String lastName = row.get("Etternavn");
 				String admission = row.get("Jeg melder meg på følgende del- utdanning");
-				String courseName = admission.split("–|\\.")[0].toLowerCase().replace(" ", "");
+				String workStatus = row.get("Arbeidsstatus");
+				String branch = row.get("Bransjetilhørighet");
+				String lastName = row.get("Etternavn");
+				String firstName = row.get("Fornavn");
+				String address = row.get("Adresse privat");
+				String postal = row.get("Postnr og sted (Skriv begge deler)");
+				String years = row.get("Antall år i bransjen");
+				String phone = row.get("Tlf nr");
+				String birth = row.get("Fødselsdato ");
+				String comment = row.get("Kommentarer");
 
-				studentService.getUserByName(firstName, lastName).ifPresentOrElse(student -> {
-					
-					
+				String courseName = admission.split("–|\\,")[0].toLowerCase().replace(" ", "");
+
+				Optional<Student> optStudent = studentService.getUserByEmail(email);
+				if (optStudent.isEmpty()) optStudent = studentService.getUserByName(firstName, lastName);
+
+				// Check if student exists
+				if (optStudent.isPresent()) {
+					Student student = optStudent.get();
+
 					Set<RoomLink> enrollments = student.getEnrollments();
 					Optional<RoomLink> enrollment = enrollments.stream().filter(match -> {
 						String name = match.getCourse().getLongName().toLowerCase();
 						name = name.replace(" ", "");
 						name = name.replace("h20", "");
+						name = name.replace("v20", "");
+						name = name.replace("s20", "");
 						name = name.replace("bransjeprogram", "");
 						return courseName.startsWith(name);
-						
-						
+
 					}).findFirst();
 
+					// Already offered
 					if (enrollment.isPresent()) {
-						
+						printer.printRecord(time, email, admission, workStatus, branch, lastName, firstName, address
+								, postal, years, phone, birth, "Har fått tilbud", comment);
+						continue;
 					}
-					else {
+				}
 
-					}
-
-
-				}, () -> {
-					printer.printRecords(row);
-				});
-
-			});
-
+				// On wait-list
+				if (time != null && time.length() > 2) {
+					printer.printRecord(time, email, admission, workStatus, branch, lastName, firstName, address
+							, postal, years, phone, birth, "På venteliste", comment);
+				}
+			}
 
 			parser.close();
 			printer.close();
-			in.close();
 			out.close();
 
 
