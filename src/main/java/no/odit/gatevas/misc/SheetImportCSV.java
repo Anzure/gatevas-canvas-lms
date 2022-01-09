e992fbd48fd1f7977708cce0b57e64a22e22918f
André Mathisen <9752808+anzure@users.noreply.github.com>
2022-01-09 12:39:09 +0100

    🛠 Importing home address.

--- src/main/java/no/odit/gatevas/misc/SheetImportCSV.java
+++ src/main/java/no/odit/gatevas/misc/SheetImportCSV.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.odit.gatevas.model.CourseType;
 import no.odit.gatevas.model.Student;
 import no.odit.gatevas.service.CourseService;
+import no.odit.gatevas.service.HomeAddressService;
 import no.odit.gatevas.service.PhoneService;
 import no.odit.gatevas.service.StudentService;
 import org.apache.commons.csv.CSVFormat;
@@ -37,6 +38,9 @@ public class SheetImportCSV {
 
     @Autowired
     private CourseService courseService;
+
+    @Autowired
+    private HomeAddressService homeAddressService;
 
     @SneakyThrows
     public Set<Student> processSheet(File csvFile, CourseType courseType, Charset charset, boolean useComma) {
@@ -75,7 +79,9 @@ public class SheetImportCSV {
                 }
 
                 if (student.getBirthDate() == null) {
-                    String birthInput = record.get("Fødselsdato");
+                    String birthInput = record.isMapped("Fodselsdato") ? record.get("Fodselsdato") : record.get("Fødselsdato");
+                    birthInput = birthInput.replaceAll("[^\\d]", "");
+                    if (birthInput.length() == 8) birthInput = birthInput.substring(0, 4) + birthInput.substring(6, 8);
                     if (birthInput.length() == 5) birthInput = "0" + birthInput;
                     LocalDate birthDate = LocalDate.parse(birthInput, DateTimeFormatter.ofPattern("ddMMyy"));
                     if (birthDate.isAfter(LocalDate.now().minusYears(15))) birthDate = birthDate.minusYears(100);
@@ -84,6 +90,31 @@ public class SheetImportCSV {
                     if (student.getBirthDate() == null) {
                         throw new Error("Failed to parsing birth date for " + student.getFullName() + "."
                                 + " Birth date input: '" + birthInput + "'");
+                    }
+                }
+
+                if (record.isMapped("Adresse")) {
+                    try {
+                        String streetAddress = record.get("Adresse");
+                        if (streetAddress != null && streetAddress.length() > 0) {
+                            if (record.isMapped("Postnummer") && record.isMapped("Sted")) {
+                                String zipInput = record.get("Postnummber");
+                                String cityName = record.get("Sted").replaceAll("[^A-Za-z]", "");
+                                Integer zipCode = Integer.parseInt(zipInput.replaceAll("[^\\d.]", ""));
+                                homeAddressService.updateHomeAddress(student, streetAddress, zipCode, cityName);
+
+                            } else if (record.isMapped("Poststed")) {
+                                String zipInput = record.get("Poststed");
+                                String cityName = zipInput.replaceAll("[^A-Za-z]", "");
+                                Integer zipCode = Integer.parseInt(zipInput.replaceAll("[^\\d.]", ""));
+                                homeAddressService.updateHomeAddress(student, streetAddress, zipCode, cityName);
+                            }
+                        }
+
+                    } catch (Exception ex) {
+                        if (student.getHomeAddress() == null) {
+                            log.warn("Invalid home address for " + firstName + " " + lastName + ".");
+                        }
                     }
                 }
 
